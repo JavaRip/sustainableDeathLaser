8f4b57aa32401c1ac3def967b6b644ecedbedd9e
JavaRip <55788902+JavaRip@users.noreply.github.com>
2019-11-03 15:00:25 +0000

    it is complete
    how the flip do you upload folders to github

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 /*frame count used to define how events happen, such as
 generating a red cell that must be clicked every 1 frames vs
 moving the laser bar every 2 frames */
-const laserUpdate = 2; //update laser every "x" frames
-const redCellUpdate = 1; //update red cells every "x" frames
+let laserUpdate = 90; //update laser every "x" frames
+let redCellUpdate = 60; //update red cells every "x" frames
 let frameNum = 0;
 //initiate game speed (in milliseconds)
-const tickSpeed = 500;
+const tickSpeed = 20;
 //initiate field (x * y)
-const fieldXMax = 10;
-const fieldYMax = 5;
+const fieldXMax = 20;
+const fieldYMax = 10;
 let field = initiateField();
 setInterval(main, tickSpeed); // calling main every frame
 //initiate variable characters
 const clearCellChar = "-", redCellChar = "R", laserChar = "B"; //console display
-const clearCellCol = "grey", redCellCol = "red", laserCol = "blue"; //canvas display
+const clearCellCol = "black", redCellCol = "lime", laserCol = "red"; //canvas display
 //initiate canvas
 let canvas = document.querySelector("canvas");
 let pixelSize = 500 / fieldXMax;
@@ -25,19 +25,65 @@ document.getElementById("laserfield-table").innerHTML = initiateTableHTML();
 //initiate laser bar
 let laserPos = 1; // must be between 0 and fieldX
 let laserDir = 1; // must be 1 or -1
+let prevLaserDir = 1;
 //initiate score 
 let redCellsCleared = 0;
 let redCellsLasered = 0;
+let startingLives = 10;
+let currentLives = 0;
+let gameOver = false;
+let prevScoreUpdate = 0; //variable to make sure game parameters only update once per score
+let score = 0;
+let laserDirChanges = 0;
+let bonusLives = 0;
+let bonusLivesUpdatedAt = 0;
+//set game type
+let gameType = "standard";
 
 //functions
 function main() {
-    //get input
-    //generate field
-    generateField();
-    //display the field
-    displayField();
-    //update frame count
-    frameNum += 1;
+    if (gameOver === false) {
+        document.getElementById("frame-count").innerHTML = frameNum;
+        //generate field
+        generateField();
+        //display the field
+        displayField();
+        //update the score
+        doGameMode();
+        //update frame count
+        frameNum += 1;
+    }
+}
+
+function doGameMode() {
+    if (gameType === "standard") {
+
+        //for every 5 points increase laser speed and redcell spawn rate
+        if (laserDir !== prevLaserDir) {
+            laserUpdate = Math.ceil(laserUpdate * 0.80);
+            prevLaserDir = laserDir;
+            laserDirChanges += 1;
+        }
+        if (redCellsCleared % 20 === 0 && bonusLivesUpdatedAt !== redCellsCleared) {
+            bonusLives += 1;
+            bonusLivesUpdatedAt = redCellsCleared;
+        }
+        if (redCellsCleared % 10 === 0 && prevScoreUpdate !== redCellsCleared) {
+            redCellUpdate = Math.ceil(redCellUpdate * 0.95);
+            prevScoreUpdate = redCellsCleared;
+        }
+        score = redCellsCleared + (laserDirChanges * 20);
+        currentLives = startingLives - redCellsLasered + bonusLives;
+        document.getElementById("player-lives").innerHTML = "Lives left <br>" + currentLives;
+        document.getElementById("laser-score").innerHTML = "Laser score <br>" + redCellsLasered;
+        document.getElementById("player-score").innerHTML = "Your score <br>" + score;
+        document.getElementById("difficulty-params").innerHTML = "laserup: " + laserUpdate
+         + "," 
+         + "redCellUp" + redCellUpdate;
+        if (currentLives <= 0) {
+            gameOver = true;
+        }
+    }
 }
 
 function generateField() {
@@ -76,9 +122,18 @@ function updateLaserPos() {
 }
 
 function addRedCell() {
+    let availibleCell = false;
     let newRedCell = false;
     let newRedCellX = 0;
     let newRedCellY = 0;
+    //if there is no space for another red cell game over
+    for (let row = 0; row < fieldYMax; row++) {
+        for (let col = 0; col < fieldXMax; col++) {
+            if (field[row][col] === clearCellChar) {
+                availibleCell = true;
+            }
+        }
+    }
     //if time to update add new red cell
     if (frameNum % redCellUpdate === 0) {
         do {
@@ -89,7 +144,7 @@ function addRedCell() {
                 field[newRedCellY][newRedCellX] = redCellChar;
                 newRedCell = true;
             }
-        } while (newRedCell === false)     
+        } while (newRedCell === false && availibleCell === true)     
     }
 }
 
@@ -140,7 +195,25 @@ function initiateField() {
     return retField;
 }
 
+function restartGame (event) {
+    bonusLives = 0;
+    laserDirChanges = 0;
+    frameNum = 0;
+    laserPos = 1;
+    laserUpdate = 90;
+    redCellUpdate = 30;
+    redCellsLasered = 0;
+    redCellsCleared = 0;
+    gameOver = false;
+    for (let row = 0; row < fieldYMax; row++) {
+        for (let col = 0; col < fieldXMax; col++) {
+            field[row][col] = clearCellChar;
+        }
+    }
+}
+
 function initiateTableHTML() {
+    //the html table takes click input to adjust the array displayed by the canvas
     let tableStr = "";
     for (let row = 0; row < fieldYMax; row++) {
         tableStr = tableStr + "<tr>";
@@ -155,6 +228,7 @@ function initiateTableHTML() {
 }
 
 function tableCellClicked(event) {
+    //this doesn't need to be called from main as it runs every time the table is clicked
     //get coords of cell that was clicked
     let id = event.srcElement.id;
     let clickCoord = "";
